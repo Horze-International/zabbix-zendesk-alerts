5c74394eef4d55b3ad228d11d7b5677712fe5f2f
David Skoland <davidskoland@gmail.com>
2020-02-03 16:27:12 +0100

    make sure solve_ticket uses json as well

--- zendesk_alert.py
+++ zendesk_alert.py
@@ -25,32 +25,33 @@ def map_priority(zabbix_severity):
 		return 'normal'
 
 zabbix_body = json.loads(json_payload)
-ticket_body = {}
-ticket_body['tags'] = ['Zabbix']
 
 if 'event_name' not in zabbix_body.keys():
 	# not recoverable.. you need at least the event name
 	print('Error: event_name missing from JSON object')
 	exit(-1)
-else:
+
+if action == 'create_ticket':
+	ticket_body = {}
+	ticket_body['tags'] = ['Zabbix']
+
 	ticket_body['subject'] = zabbix_body['event_name']
 	ticket_body['description'] = zabbix_body['event_name']
 
-if 'event_severity' in zabbix_body.keys():
-	ticket_body['priority'] = map_priority(zabbix_body['event_severity'])
+	if 'event_severity' in zabbix_body.keys():
+		ticket_body['priority'] = map_priority(zabbix_body['event_severity'])
 
-if 'host_name' in zabbix_body.keys():
-	ticket_body['tags'].append(zabbix_body['host_name'])
+	if 'host_name' in zabbix_body.keys():
+		ticket_body['tags'].append(zabbix_body['host_name'])
 
-zp_client = zp.Zenpy(**c.configurations[zendesk_subdomain]['zendesk_instance'])
+	zp_client = zp.Zenpy(**c.configurations[zendesk_subdomain]['zendesk_instance'])
 
-ticket_body['requester'] = zp_client.users(id=int(c.configurations[zendesk_subdomain]['zabbix_user_id']))
+	ticket_body['requester'] = zp_client.users(id=int(c.configurations[zendesk_subdomain]['zabbix_user_id']))
 
-if action == 'create_ticket':
 	ticket = zp.lib.api_objects.Ticket(**ticket_body)
 	zp_client.tickets.create(ticket)
 elif action == 'solve_ticket':
-	for ticket in zp_client.search(type='ticket',status_less_than='solved',subject=message):
+	for ticket in zp_client.search(type='ticket',status_less_than='solved',subject=zabbix_body['event_name']):
 		ticket.status = 'solved'
 		zp_client.tickets.update(ticket)
 else:
