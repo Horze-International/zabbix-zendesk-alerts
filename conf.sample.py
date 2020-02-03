51ef5d2d5acdb6893d771d036cc521b6965ee4b2
David Skoland <davidskoland@gmail.com>
2020-02-03 17:07:47 +0100

    added zabbix_user_id in sample conf

--- conf.sample.py
+++ conf.sample.py
@@ -4,6 +4,7 @@ configurations = {
 			'email': 'john.doe@example.com',
 			'token': 'your_zendesk_token',
 			'subdomain': 'your_zendesk_subdomain'
-		}
+		},
+		'zabbix_user_id': '1234567890'
 	}
 }
