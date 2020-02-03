701ce5af39db3c0757f1ef1a64768a33c49baf54
David Skoland <davidskoland@gmail.com>
2020-02-03 15:51:19 +0100

    major rework, use JSON objects to aggregate information from zabbix

--- conf.sample.py
+++ conf.sample.py
@@ -1,7 +1,9 @@
-zendesk_instances = {
+configurations = {
 	'your_zendesk_subdomain': {
-		'email': 'john.doe@example.com',
-		'token': 'your_zendesk_token',
-		'subdomain': 'your_zendesk_subdomain'
+		'zendesk_instance': {
+			'email': 'john.doe@example.com',
+			'token': 'your_zendesk_token',
+			'subdomain': 'your_zendesk_subdomain'
+		}
 	}
 }
--- zendesk_alert.py
+++ zendesk_alert.py
@@ -1,21 +1,53 @@
-#!/usr/bin/python3
-
+#!/usr/local/bin/python3
+import json
 import conf as c
 import zenpy as zp
 import sys
 
 if len(sys.argv) < 4:
-	print('Error: expected three arguments: Zendesk instance, action and trigger message')
+	print('Error: expected three arguments: Zendesk subdomain, action and trigger message')
 	exit(-1)
 
-zendesk_instance = sys.argv[1]
+zendesk_subdomain = sys.argv[1]
 action = sys.argv[2]
-message = sys.argv[3]
+json_payload = sys.argv[3]
 
-zp_client = zp.Zenpy(**c.zendesk_instances[zendesk_instance])
+def map_priority(zabbix_severity):
+	if zabbix_severity == 'Warning':
+		return 'low'
+	elif zabbix_severity == 'Average':
+		return 'normal'
+	elif zabbix_severity == 'High':
+		return 'high'
+	elif zabbix_severity == 'Disaster':
+		return 'urgent'
+	else:
+		return 'normal'
+
+zabbix_body = json.loads(json_payload)
+ticket_body = {}
+ticket_body['tags'] = ['Zabbix']
+
+if 'event_name' not in zabbix_body.keys():
+	# not recoverable.. you need at least the event name
+	print('Error: event_name missing from JSON object')
+	exit(-1)
+else:
+	ticket_body['subject'] = zabbix_body['event_name']
+	ticket_body['description'] = zabbix_body['event_name']
+
+if 'event_severity' in zabbix_body.keys():
+	ticket_body['priority'] = map_priority(zabbix_body['event_severity'])
+
+if 'host_name' in zabbix_body.keys():
+	ticket_body['tags'].append(zabbix_body['host_name'])
+
+zp_client = zp.Zenpy(**c.configurations[zendesk_subdomain]['zendesk_instance'])
+
+ticket_body['requester'] = zp_client.users(id=int(c.configurations[zendesk_subdomain]['zabbix_user_id']))
 
 if action == 'create_ticket':
-	ticket = zp.lib.api_objects.Ticket(subject=message, description=message, requester=zp.lib.api_objects.User(name='Zabbix'))
+	ticket = zp.lib.api_objects.Ticket(**ticket_body)
 	zp_client.tickets.create(ticket)
 elif action == 'solve_ticket':
 	for ticket in zp_client.search(type='ticket',status_less_than='solved',subject=message):
